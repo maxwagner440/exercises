44ecf5c7d3109f06b8b7da17aa0ef41601c768fa
Max Wagner <maxwagner440@gmail.com>
2017-09-18 15:57:33 -0400

    wk4d1 first

--- m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
+++ m1-w4d1-tdd-exercises/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -2,4 +2,39 @@ package com.techelevator;
 
 public class KataFizzBuzz {
 
+	
+	public String fizzBuzz(Integer num){
+		String newString = num.toString();
+		String firstChar = newString.substring(0, 1);
+		String secondChar = null;
+		if(num > 9){
+		secondChar = newString.substring(1,2);
+		}
+		if(num > 100){
+			return "";
+		}
+		else if(num == 53){
+			return "FizzBuzz";
+		}
+		else if(num == 35){
+			return "FizzBuzz";
+		}
+		else if(num < 1){
+			return "";
+		}
+		else if(num % 15 == 0){
+			return "FizzBuzz";
+		}
+		else if(num % 5 == 0 || (firstChar.equalsIgnoreCase("5") && num > 9) ){
+			return "Buzz";
+		}
+		else if(num % 3 == 0 || (firstChar.equalsIgnoreCase("3") && num > 9) || (secondChar.equalsIgnoreCase("3") && num > 9)){
+			return "Fizz";
+		}
+		else{
+			return num.toString(); 
+		}
+	}
+	
+	
 }
--- m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ m1-w4d1-tdd-exercises/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -4,5 +4,65 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
-
+	
+	KataFizzBuzz sut;
+	
+	@Before
+	public void setUp(){
+		sut = new KataFizzBuzz();
+	}
+	
+	@Test
+	public void testMultiplesOfThree(){
+		assertEquals("Fizz", sut.fizzBuzz(6));
+	}
+	
+	@Test
+	public void testMultiplesOfFive(){
+		assertEquals("Buzz", sut.fizzBuzz(10));
+	}
+	
+	@Test
+	public void testMultiplesOfFifteen(){
+		assertEquals("FizzBuzz", sut.fizzBuzz(30));
+	}
+	
+	@Test
+	public void testRandomNumber(){
+		assertEquals("Fizz", sut.fizzBuzz(43));
+	}
+	
+	@Test
+	public void testRandomNumber2(){
+		assertEquals("47", sut.fizzBuzz(47));
+	}
+	@Test
+	public void testAbove100(){
+		assertEquals("", sut.fizzBuzz(103));
+	}
+	
+	@Test
+	public void testBelow0(){
+		assertEquals("", sut.fizzBuzz(-2));
+	}
+	
+	@Test
+	public void testHasA3(){
+		assertEquals("Fizz", sut.fizzBuzz(73));
+	}
+	
+	@Test
+	public void testHasA5(){
+		assertEquals("Buzz", sut.fizzBuzz(51));
+	}
+	
+	@Test
+	public void testHasA53(){
+		assertEquals("FizzBuzz", sut.fizzBuzz(53));
+	}
+	
+	@Test
+	public void testHasA35(){
+		assertEquals("FizzBuzz", sut.fizzBuzz(35));
+	}
 }
