a7e3d95634536e42e23210f1c3bf1a2d7655f602
Max Wagner <maxwagner440@gmail.com>
2017-11-01 17:20:40 -0400

    done

--- m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
+++ m4-w10d2-java-history_geek_exercises/src/main/webapp/js/exercises/event-exerises.js
@@ -2,4 +2,44 @@
 
 $(document).ready(function () {
 
+	$("#SameShipping").on('click', function(){
+		if(this.checked){
+			$("#ShippingAddress1").val($("#BillingAddress1").val());
+			$("#ShippingAddress2").val($("#BillingAddress2").val());
+			$("#ShippingCity").val($("#BillingCity").val());
+			$("#ShippingState").val($("#BillingState").val());
+			$("#ShippingPostalCode").val($("#BillingPostalCode").val());
+		}
+		else{
+			$("#ShippingAddress1").val("");
+			$("#ShippingAddress2").val("");
+			$("#ShippingCity").val("");
+			$("#ShippingState").val("");
+			$("#ShippingPostalCode").val("");
+		}
+	});
+	
+	$('input[name=ShippingType]').on('change', function(){
+		var shippingAmount = $("input[name=ShippingType]:checked").attr('data-cost');
+		$('#order-summary #shipping span').text('$' + shippingAmount);
+		grandTotal();
+	});
+	
+	
+	
 });
+
+function grandTotal(){
+	var subtotal = $('#subtotal span').text();
+	subtotal = parseFloat(subtotal.substring(1, subtotal.length));
+	
+	var tax = $('#tax span').text();
+	tax = parseFloat(tax.substring(1, tax.length));
+	
+	var shipping = $('#shipping span').text();
+	shipping = parseFloat(shipping.substring(1, shipping.length));
+	
+	var grandTotal = subtotal + tax + shipping;
+	$('#order-summary #grandtotal span').text('$' + grandTotal);
+	
+}
